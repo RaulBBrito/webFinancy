7c7eb2733d6d346475a494bde0021969a144118d
Raul Brito <raulbbrito@hotmail.com>
2023-05-01 20:59:04 -0300

    Tela Login Finalizada - Com Token sessão

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,92 +13,35 @@ import { Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
- public isLogin: boolean = true;
-
-  @ViewChild('formLogin') formLogin!: NgForm;
   login: Login = new Login();
-  usuario: Usuario = new Usuario();
-  loading: boolean = false;
-  message!:string;
 
   formulario!: FormGroup;
-  formularioCadastro!: FormGroup;
-
-  formularioLogin: Login = {
-    email: 'raulbbrito@hotmail.com',
-    password: '513482Am@'
-
-  }
 
   constructor(private formBuilder: FormBuilder,
     private loginService: LoginService,
-    private router: Router,
-    private route: ActivatedRoute
+    private router: Router
   ) { 
-    if(this.loginService.usuarioLogado){
+    if(this.loginService.getUsuarioLogado()){
       this.router.navigate(["/home"]);
     }
   }
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      email: ['email@email.com', [Validators.required, Validators.email]],
-      password: ['123adad@@', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-
-    this.formularioCadastro = this.formBuilder.group({
-      nome: ['Raul Brito', [Validators.required]],
-      email: ['raulbbrito@hotmail.com', [Validators.required, Validators.email]],
-      password: ['513482Am@', [Validators.required, Validators.minLength(6)]],
-    });
-
-    /*this.route.queryParams
-    .subscribe(params =>{
-      this.message = params['error'];
-    })*/
   }
 
-  getPagina(){
-    this.isLogin = !this.isLogin;
-  }
 
   logar(): void {
-    this.loading = true;
     if(this.formulario.valid){
-      this.loginService.login(this.formularioLogin).subscribe((usu) => {
-        if(usu != null) {
-
-          let usu = new Usuario(null, 'Raul Brito ', 'email@email', '', "ADMIN");
-          
-          this.loginService.usuarioLogado = usu;
-          this.loading = false;
-          this.router.navigate(["home"]);
-        }else{
-          this.loading = false;
-          this.message = "Usuario/Senha inválidos"
-        }
-      }
-
-      )
+      this.loginService.login({email: this.formulario.value.email, password: this.formulario.value.password})
+      .subscribe((usu) => (usu != null) ? this.router.navigate(["home"]) : null)
     }
   }
 
   cadastrar(): void {
-    this.loading = true;
-    if(this.formularioCadastro.valid){
-      this.loginService.cadastrar(this.usuario).subscribe((usu) => {
-        if(usu != null) {
-          this.loginService.usuarioLogado = usu;
-          this.loading = false;
-          this.router.navigate(["home"]);
-        }else{
-          this.loading = false;
-          this.message = "Usuario/Senha inválidos"
-        }
-      }
-
-      )
-    }
   }
 
   redirect(){
