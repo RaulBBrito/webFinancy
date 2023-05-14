d1faea139b7e097fc893e6cd14881dab644fba10
Raul Brito <raulbbrito@hotmail.com>
2023-05-14 10:11:52 -0300

    Tela Home - Resumo Valores

--- src/app/pages/home/components/relatorio/relatorio.component.ts
+++ src/app/pages/home/components/relatorio/relatorio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '@app/auth/services/login.service';
+import { IResumo } from '@app/core/interfaces';
 
 @Component({
   selector: 'app-relatorio',
@@ -10,6 +11,9 @@ import { LoginService } from '@app/auth/services/login.service';
 export class RelatorioComponent implements OnInit {
 
   usuarioSessao: any;
+
+  resumoRendas: IResumo = {}
+  resumoDespesas: IResumo = {}
 
   constructor(
     private router: Router,
@@ -23,10 +27,24 @@ export class RelatorioComponent implements OnInit {
     }
 
     ngOnInit(): void {
+      this.popularResumo();
       this.usuarioSessao = this.loginService.getUsuarioLogado();
       if(this.usuarioSessao == null){
         this.logout();
       }
     }
 
+    popularResumo(){
+      this.resumoRendas = {
+        efetuadoRecebido: {descricao:'Rendas recebidas', valor: 'R$ 748,00'},
+        aguardando: {descricao:'Aguardando recebimento', valor: 'R$ 1930,00'},
+        valorTotal: {descricao:'Valor total da renda', valor: 'R$ 2.678,00'},
+      }
+
+      this.resumoDespesas = {
+        efetuadoRecebido: {descricao:'Pagamento efetuado', valor: 'R$ 280,27'},
+        aguardando: {descricao:'Aguardando pagamento', valor: 'R$ 746,65'},
+        valorTotal: {descricao:'Valor total das despesas', valor: 'R$ 1.026,92'},
+      }
+    }
 }
